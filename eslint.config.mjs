c019c9b605419f58bed64d172fdd616dd577d929
manxzo <manzoball@gmail.com>
2025-03-30 00:26:08 +0800

    updated home page and added reactbits components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "no-unused-labels": "off",
+      "no-unreachable": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
